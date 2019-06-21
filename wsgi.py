007f6b58c9be57ee3412a7f5f662f25f80e0345c
Guillaume Allard <guillaume.allard@gmail.com>
2019-06-21 12:00:46 +0200

    Add admin page

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
 from flask import Flask, abort, request, render_template
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from config import Config
 
 app = Flask(__name__)
@@ -11,6 +13,9 @@ ma = Marshmallow(app)
 
 from models import Product
 from schemas import products_schema, product_schema
+
+admin = Admin(app, template_mode='bootstrap3')
+admin.add_view(ModelView(Product, db.session))
 
 @app.route('/')
 def home():
